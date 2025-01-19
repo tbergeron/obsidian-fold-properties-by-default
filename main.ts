7f41aa259a7728fee9633a606b1c1587ca7f3078
Tommy Bergeron <tom@brainpad.org>
2025-01-20 00:39:26 +0800

    it works!

--- main.ts
+++ main.ts
@@ -1,35 +1,17 @@
 import { Plugin } from 'obsidian'
 
-interface FoldedProperties {
-	folds: { from: number, to: number }[];
-}
-
 export default class FoldPropertiesByDefault extends Plugin {
 	foldProperties() {
 		const app = this.app as any
-
-		const file = this.app.workspace.getActiveFile()
-		if (!file) {
-			return
-		}
-
-        const existingFolds: FoldedProperties | null = app.foldManager.loadPath(
-            file.path
-        )
-
-		console.warn('existingFolds', existingFolds, 'isPropertiesFolded', existingFolds ? this.isPropertiesFolded(existingFolds) : null)
-
-		// TODO: need to either find a different command or implement a way to check if it's already folded
-		if (existingFolds && !this.isPropertiesFolded(existingFolds)) {
-			app.commands.executeCommandById('editor:toggle-fold-properties')
+		const currentLeaf = document.querySelector('.workspace-leaf.mod-active')
+		if (currentLeaf) {
+			const propertiesAreFolded = currentLeaf.querySelector('.metadata-container.is-collapsed')
+			if (!propertiesAreFolded) {
+				console.warn('folding properties')
+				app.commands.executeCommandById('editor:toggle-fold-properties')
+			}
 		}
 	}
-
-    isPropertiesFolded(existingFolds: FoldedProperties) {
-        return existingFolds.folds.some(
-            (item) => item.from == 0 && item.to == 0
-        )
-    }
 
 	async onload() {
 		this.registerEvent(this.app.workspace.on('file-open', this.foldProperties.bind(this)))
