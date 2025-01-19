90de372f4081b4747c1f1c23fc594de005d0829d
Tommy Bergeron <tom@brainpad.org>
2025-01-19 23:34:09 +0800

    test

--- main.ts
+++ main.ts
@@ -1,20 +1,18 @@
 import { Plugin } from 'obsidian'
 
-interface UnsafeAppInterface {
-  commands: {
-	commands: { [commandId: string]: { id: string, name: string, callback: () => void } }
-    executeCommandById(commandId: string): boolean
+declare module 'obsidian' {
+  interface App {
+	commands: {
+		commands: { [commandId: string]: { id: string, name: string, callback: () => void } }
+		executeCommandById(commandId: string): boolean
+	}
   }
 }
 
 export default class FoldPropertiesByDefault extends Plugin {
-
-	// fold properties function
 	foldProperties() {
-		const unsafeApp = this.app as unknown as UnsafeAppInterface
-		unsafeApp.commands.executeCommandById('editor:toggle-fold-properties')
+		this.app.commands.executeCommandById('editor:toggle-fold-properties')
 	}
-
 	async onload() {
 		this.registerEvent(this.app.workspace.on("file-open", this.foldProperties.bind(this)))
 	}
